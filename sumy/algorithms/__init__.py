008def6cdd66ec7bb0c46db8aadc7a113d49902c
Mišo Belica <miso.belica@gmail.com>
2013-03-04 22:38:36 +0100

    Every summarization in separate public package
    This is done for consistency with LSA method.
    This method is not imported in
    'sumy.algorithms.__init__' file because
    'numpy' & 'scipy' requirements are optional
    so user who want to use other method don't
    need to install this requirements.

--- sumy/algorithms/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf8 -*-
-
-from __future__ import absolute_import
-from __future__ import division, print_function, unicode_literals
-
-from ._luhn import LuhnMethod
-from ._edmundson import EdmundsonMethod
