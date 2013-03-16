900461fc8e77753cf97e926baac797cf5150fb74
Mišo Belica <miso.belica@gmail.com>
2013-03-16 21:08:19 +0100

    Python 3 is not supported yet :(

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
+        # "Programming Language :: Python :: 3",
+        # "Programming Language :: Python :: 3.3",
     ),
 )
