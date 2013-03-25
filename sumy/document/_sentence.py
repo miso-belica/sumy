41659cf49b92e9416863364a85638ac36f09657d
Mišo Belica <miso.belica@gmail.com>
2013-03-25 23:08:08 +0100

    Better '__repr__' for 'Sentence'

--- sumy/document/_sentence.py
+++ sumy/document/_sentence.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
 from itertools import chain
-from .._compat import to_unicode, unicode_compatible
+from .._compat import to_unicode, to_string, unicode_compatible
 
 
 @unicode_compatible
@@ -25,3 +25,6 @@ class Sentence(object):
 
     def __unicode__(self):
         return " ".join(self._words)
+
+    def __repr__(self):
+        return to_string("<Sentence: %s>") % self.__str__()
