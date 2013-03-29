5553fbdd420046572526bdb745c67ad545c03446
Mišo Belica <miso.belica@gmail.com>
2013-03-29 15:01:14 +0100

    Updated implementation of cached property

--- sumy/utils.py
+++ sumy/utils.py
@@ -5,6 +5,7 @@ from __future__ import division, print_function, unicode_literals
 
 import sys
 
+from functools import wraps
 from os.path import dirname, abspath, join
 from ._compat import to_string
 
@@ -12,21 +13,17 @@ from ._compat import to_string
 def cached_property(getter):
     """
     Decorator that converts a method into memoized property.
-    The decorator works as expected only for immutable properties.
+    The decorator works as expected only for classes with
+    attribute '__dict__' and immutable properties.
     """
+    @wraps(getter)
     def decorator(self):
-        if not hasattr(self, "__cached_property_data"):
-            self.__cached_property_data = {}
+        key = "_cached_property_" + getter.__name__
 
-        key = getter.__name__
-        if key not in self.__cached_property_data:
-            self.__cached_property_data[key] = getter(self)
+        if not hasattr(self, key):
+            setattr(self, key, getter(self))
 
-        return self.__cached_property_data[key]
-
-    decorator.__name__ = getter.__name__
-    decorator.__module__ = getter.__module__
-    decorator.__doc__ = getter.__doc__
+        return getattr(self, key)
 
     return property(decorator)
 
