3c313405649e47884c99340120fb55ef30405b24
Mišo Belica <miso.belica@gmail.com>
2013-04-05 23:38:44 +0200

    Added function for getting stopwords

--- sumy/utils.py
+++ sumy/utils.py
@@ -6,8 +6,8 @@ from __future__ import division, print_function, unicode_literals
 import sys
 
 from functools import wraps
-from os.path import dirname, abspath, join
-from ._compat import to_string
+from os.path import dirname, abspath, join, exists
+from ._compat import to_string, to_unicode, string_types
 
 
 def cached_property(getter):
@@ -32,3 +32,12 @@ def expand_resource_path(path):
     directory = dirname(sys.modules["sumy"].__file__)
     directory = abspath(directory)
     return join(directory, to_string("data"), to_string(path))
+
+
+def get_stop_word(language):
+    path = expand_resource_path("stopwords/%s.txt" % language)
+    if not exists(path):
+        raise ValueError("Stop-words are not available for language %s." % language)
+
+    with open(path, "rb") as file:
+        return frozenset(to_unicode(w.rstrip()) for w in file.readlines())
