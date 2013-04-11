4c7525291248b1c2ac9e40d277a25436478a2332
Mišo Belica <miso.belica@gmail.com>
2013-04-11 16:38:07 +0200

    LSA: added "real-world" summarization test

--- tests/test_summarizers/test_lsa.py
+++ tests/test_summarizers/test_lsa.py
@@ -6,6 +6,10 @@ from __future__ import division, print_function, unicode_literals
 import unittest
 
 from sumy.summarizers.lsa import LsaSummarizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.nlp.stemmers.cs import stem_word
+from sumy.utils import get_stop_words
 from sumy._compat import to_unicode
 from ..utils import build_document
 
@@ -39,3 +43,23 @@ class TestLsa(unittest.TestCase):
         self.assertEqual(len(sentences), 2)
         self.assertEqual(to_unicode(sentences[0]), "I am the sentence you like")
         self.assertEqual(to_unicode(sentences[1]), "This sentence is better than that above")
+
+    def test_real_example(self):
+        parser = PlaintextParser.from_string(
+            "Jednalo se o případ chlapce v 6. třídě, který měl problémy s učením. "
+            "Přerostly až v reparát z jazyka na konci školního roku. "
+            "Nedopadl bohužel dobře a tak musel opakovat 6. třídu, což se chlapci ani trochu nelíbilo. "
+            "Připadal si, že je mezi malými dětmi a realizoval se tím, že si ve třídě "
+            "o rok mladších dětí budoval vedoucí pozici. "
+            "Dost razantně. Fyzickou převahu měl, takže to nedalo až tak moc práce.",
+            Tokenizer("czech")
+        )
+        summarizer = LsaSummarizer(parser.document, stem_word)
+        summarizer.stop_words = get_stop_words("cs")
+
+        sentences = summarizer(2)
+        self.assertEqual(len(sentences), 2)
+        self.assertEqual(to_unicode(sentences[0]),
+            "Jednalo se o případ chlapce v 6. třídě , který měl problémy s učením .")
+        self.assertEqual(to_unicode(sentences[1]),
+            "Nedopadl bohužel dobře a tak musel opakovat 6. třídu , což se chlapci ani trochu nelíbilo .")
