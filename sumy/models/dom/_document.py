a241a005e7cde79e05854b8d5a5cfa15daff4d87
Mišo Belica <miso.belica@gmail.com>
2013-04-22 19:02:29 +0200

    Added string representation for 'ObjectDocumentModel'

--- sumy/models/dom/_document.py
+++ sumy/models/dom/_document.py
@@ -5,8 +5,10 @@ from __future__ import division, print_function, unicode_literals
 
 from itertools import chain
 from ...utils import cached_property
+from ..._compat import unicode_compatible
 
 
+@unicode_compatible
 class ObjectDocumentModel(object):
     def __init__(self, paragraphs):
         self._paragraphs = tuple(paragraphs)
@@ -29,3 +31,9 @@ class ObjectDocumentModel(object):
     def words(self):
         words = (p.words for p in self._paragraphs)
         return tuple(chain(*words))
+
+    def __unicode__(self):
+        return "<DOM with %d paragraphs>" % len(self.paragraphs)
+
+    def __repr__(self):
+        return self.__str__()
