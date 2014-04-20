17c3465bc1dddaeab907e54de41c3fc804cc2d8f
Lucas Cimon <lucas.cimon@gmail.com>
2014-04-20 12:10:54 +0100

    Adding support for French + more concise & generic NLTK stemmers import

--- sumy/nlp/stemmers/english.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-
-from __future__ import absolute_import
-from __future__ import division, print_function, unicode_literals
-
-from nltk.stem.snowball import EnglishStemmer
-
-
-stem_word = EnglishStemmer().stem
--- tests/test_stemmers.py
+++ tests/test_stemmers.py
@@ -5,10 +5,7 @@ from __future__ import division, print_function, unicode_literals
 
 import unittest
 
-from sumy.nlp.stemmers import null_stemmer
-from sumy.nlp.stemmers.czech import stem_word as czech_stemmer
-from sumy.nlp.stemmers.english import stem_word as english_stemmer
-from sumy.nlp.stemmers.german import stem_word as german_stemmer
+from sumy.nlp.stemmers import null_stemmer, Stemmer
 
 
 class TestStemmers(unittest.TestCase):
@@ -17,10 +14,23 @@ class TestStemmers(unittest.TestCase):
         self.assertEqual("ľščťžýáíé", null_stemmer("ľŠčŤžÝáÍé"))
 
     def test_english_stemmer(self):
+        english_stemmer = Stemmer('english')
         self.assertEqual("beauti", english_stemmer("beautiful"))
 
     def test_german_stemmer(self):
+        german_stemmer = Stemmer('german')
         self.assertEqual("sterb", german_stemmer("sterben"))
 
     def test_czech_stemmer(self):
+        czech_stemmer = Stemmer('czech')
         self.assertEqual("pěkn", czech_stemmer("pěkný"))
+
+    def test_french_stemmer(self):
+        french_stemmer = Stemmer('czech')
+        self.assertEqual("jol", french_stemmer("jolies"))
+
+    def test_slovak_stemmer(self):
+        expected = Stemmer("czech")
+        actual = Stemmer("slovak")
+        self.assertEqual(type(actual), type(expected))
+        self.assertEqual(expected.__dict__, actual.__dict__)
