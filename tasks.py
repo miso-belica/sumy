0793f8dcb6ed27832e7d0adfb920d9c70813f3c7
Mišo Belica <miso.belica@gmail.com>
2016-11-12 17:43:09 +0100

    Use new invoke's context parameter

--- tasks.py
+++ tasks.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from invoke import task, run
+from invoke import task
 
 
 @task
-def clean():
-    run("rm -rf .coverage dist build")
+def clean(context):
+    context.run("rm -rf .coverage dist build")
 
 
 @task(clean, default=True)
-def test():
-    run("py.test")
+def test(context):
+    context.run("py.test")
 
 
 @task(test)
-def install():
-    run("pandoc --from=markdown --to=rst README.md -o README.rst")
-    run("python setup.py develop")
+def install(context):
+    context.run("pandoc --from=markdown --to=rst README.md -o README.rst")
+    context.run("python setup.py develop")
 
 
 @task(test)
-def release():
-    run("pandoc --from=markdown --to=rst README.md -o README.rst")
-    run("python setup.py register sdist bdist_wheel")
-    run("twine upload dist/*")
+def release(context):
+    context.run("pandoc --from=markdown --to=rst README.md -o README.rst")
+    context.run("python setup.py register sdist bdist_wheel")
+    context.run("twine upload dist/*")
 
 
 @task(test)
-def bump(version="patch"):
-    run("bumpversion %s" % version)
-    run("git commit --amend")
+def bump(context, version="patch"):
+    context.run("bumpversion %s" % version)
+    context.run("git commit --amend")
