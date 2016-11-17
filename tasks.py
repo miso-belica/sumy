0142abc6d5b6e7f4dd6aedceea87b1d3198c2e0b
Mišo Belica <miso.belica@gmail.com>
2016-11-17 11:24:02 +0100

    Trying to fix instalation from git, fixes #71
    Four people were complaining about this. I had
    no idea so many people are installing sumy
    from git instead of PyPI.

--- tasks.py
+++ tasks.py
@@ -15,18 +15,17 @@ def test(context):
 
 @task(test)
 def install(context):
-    context.run("pandoc --from=markdown --to=rst README.md -o README.rst")
     context.run("python setup.py develop")
 
 
 @task(test)
 def release(context):
-    context.run("pandoc --from=markdown --to=rst README.md -o README.rst")
     context.run("python setup.py register sdist bdist_wheel")
     context.run("twine upload dist/*")
 
 
 @task(test)
 def bump(context, version="patch"):
+    context.run("pandoc --from=markdown --to=rst README.md -o README.rst")
     context.run("bumpversion %s" % version)
     context.run("git commit --amend")
