e81bac3659f150ff55717b5aac808907e846416e
Sarthak anand <isarth@me.com>
2019-05-18 21:51:48 +0530

    Fix runtime zero division error in TextRank #112

--- sumy/summarizers/text_rank.py
+++ sumy/summarizers/text_rank.py
@@ -20,6 +20,8 @@ class TextRankSummarizer(AbstractSummarizer):
     """
     epsilon = 1e-4
     damping = 0.85
+    # small number to prevent zero-division error, see https://github.com/miso-belica/sumy/issues/112
+    _delta = 1e-7 
     _stop_words = frozenset()
 
     @property
@@ -65,7 +67,8 @@ class TextRankSummarizer(AbstractSummarizer):
         for i, words_i in enumerate(sentences_as_words):
             for j, words_j in enumerate(sentences_as_words):
                 weights[i, j] = self._rate_sentences_edge(words_i, words_j)
-        weights /= weights.sum(axis=1)[:, numpy.newaxis]
+        weights /= (weights.sum(axis=1)[:, numpy.newaxis]+self._delta) # delta added to prevent zero-division error 
+        #(see issue https://github.com/miso-belica/sumy/issues/112 )
 
         # In the original paper, the probability of randomly moving to any of the vertices
         # is NOT divided by the number of vertices. Here we do divide it so that the power
