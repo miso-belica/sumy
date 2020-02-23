77a9d9ea3750fbb9099e2e6223d072f6cfc0c187
Mišo Belica <miso.belica@gmail.com>
2020-02-23 16:01:35 +0100

    PEP8

--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -147,6 +147,5 @@ def test_tokenize_korean_paragraph():
         '강남 역 맛 집 토끼정의 외부 모습.'
     )
 
-
     paragraph = '회사 동료 분들과 다녀왔는데 분위기도 좋고 음식도 맛있었어요 다만, 강남 토끼정이 강남 쉑쉑버거 골목길로 쭉 올라가야 하는데 다들 쉑쉑버거의 유혹에 넘어갈 뻔 했답니다 강남역 맛집 토끼정의 외부 모습.'
     assert expected == tokenizer.to_sentences(paragraph)
