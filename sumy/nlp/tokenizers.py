b9c40f05b6354694494f892b9a002e4d9b5cd7db
abderrahmane issam <abderrahmanissam285@gmail.com>
2022-07-27 21:37:29 +0100

    Add arabic word tokenizer

--- sumy/nlp/tokenizers.py
+++ sumy/nlp/tokenizers.py
@@ -36,7 +36,7 @@ class HebrewWordTokenizer:
             word for token, word, _, _ in tokenize(text)
             if token in (Groups.HEBREW, Groups.HEBREW_1, Groups.HEBREW_2)
         ]
-
+    
 
 class JapaneseWordTokenizer:
     def tokenize(self, text):
@@ -96,6 +96,16 @@ class GreekSentencesTokenizer:
         return [sentence.strip() for sent_gen in sentences for sentence in sent_gen]
 
 
+class ArabicWordTokenizer:
+
+    def tokenize(self, text):
+        try:
+            from pyarabic.araby import tokenize
+        except ImportError as e:
+            raise ValueError("arabic tokenizer requires pyarabic. Please, install it by command 'pip install pyarabic'.")
+        return tokenize(text)
+
+
 class Tokenizer(object):
     """Language dependent tokenizer of text document."""
 
@@ -122,6 +132,7 @@ class Tokenizer(object):
         'chinese': nltk.RegexpTokenizer('[^　！？。]*[！？。]'),
         'korean': KoreanSentencesTokenizer(),
         'greek': GreekSentencesTokenizer(),
+        'arabic': ArabicSentenceTokenizer(),
     }
 
     SPECIAL_WORD_TOKENIZERS = {
@@ -130,6 +141,7 @@ class Tokenizer(object):
         'chinese': ChineseWordTokenizer(),
         'korean': KoreanWordTokenizer(),
         'greek': nltk.RegexpTokenizer(r"[ ,;;.!?:-]+", gaps=True),
+        'arabic': ArabicWordTokenizer(),
     }
 
     def __init__(self, language):
