1426db17466aa952e1d4c0f97a463581efda3532
Mišo <1072330+miso-belica@users.noreply.github.com>
2026-02-14 21:58:40 +0100

    Update code for v0.12

--- sumy/__init__.py
+++ sumy/__init__.py
@@ -3,5 +3,5 @@
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
-__author__ = "Michal Belica"
-__version__ = "0.11.0"
+__author__ = "Mišo Belica"
+__version__ = "0.12.0"
